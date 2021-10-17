af4c05a03fc04b2a5f7cb6f0a71237e75075e56a
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2021-10-17 11:23:25 +0300

    well done!

---
+++ example-fastapi-desktop.py
@@ -0,0 +1,13 @@
+from py_to_win_app import Project
+
+app_name = "fastapi-desktop"
+
+p = Project(
+    input_dir=f"examples/{app_name}", main_file="main.py", app_name=app_name
+)
+
+p.build(
+    python_version="3.9.7",
+    requirements_file=f"examples/{app_name}/requirements.txt",
+    exe_name=app_name,
+)
--- py_to_win_app/py_to_win_app.py
+++ py_to_win_app/py_to_win_app.py
@@ -1,20 +1,29 @@
-import zipfile
-import requests
+import os
+import re
 import shutil
+import subprocess
+import sys
+import zipfile
+from contextlib import contextmanager
 from pathlib import Path
 from typing import Iterable, Union
-import re
-from contextlib import contextmanager
+
+import requests
+from genexe.generate_exe import generate_exe
 
 PYTHON_VERSION_REGEX = re.compile(r"^(\d+|x)\.(\d+|x)\.(\d+|x)$")
 GET_PIP_URL = "https://bootstrap.pypa.io/get-pip.py"
 PYTHON_URL = "https://www.python.org/ftp/python"
-HEADER_NO_CONSOLE = """import sys, os
-if sys.executable.endswith('pythonw.exe'):
-    sys.stdout = open(os.devnull, 'w')
-    sys.stderr = (Path(__file__).parent / 'stderr').open('w')
+HEADER_NO_CONSOLE = (
+    """import sys, os"""
+    + """\nif sys.executable.endswith('pythonw.exe'):"""
+    + """\n    sys.stdout = open(os.devnull, 'w')"""
+    + """\n    sys.stderr = open(os.path.join(os.getenv(\'TEMP\'), """
+    + """\'stderr-{}\'.format(os.path.basename(sys.argv[0]))), "w")"""
+    + """\n\n"""
+)
 
-"""
+
 DEFAULT_IGNORE_PATTERNS = [
     "__pycache__",
     "*.pyc",
@@ -36,6 +45,7 @@ class Project:
     ) -> None:
         self._path = Path().cwd()
         self._input_path = self._path / input_dir
+        self._main_file = main_file
         self._app_name = app_name if app_name is not None else self._path.name
 
         (self._path / "build").mkdir(exist_ok=True)
@@ -89,18 +99,38 @@ class Project:
         return re.match(PYTHON_VERSION_REGEX, python_version)
 
     @staticmethod
-    def get_short_version(python_version: str) -> str:
+    def _get_short_version(python_version: str) -> str:
         return "".join(python_version.split(".")[:2])  # 3.9.7 -> 39
 
-    def _make_empty_build_dir(self) -> None:
-        # Delete build folder if it exists
-        if self._build_path.is_dir():
-            with log(
-                f"Existing build directory found, "
-                f"removing contents from `{self._build_path}`"
-            ):
-                shutil.rmtree(self._build_path)
-        self._build_path.mkdir()
+    @staticmethod
+    def _execute_os_command(command: str, cwd: str = None) -> str:
+        """Execute terminal command"""
+
+        with log(f"Running command: {command}"):
+            process = subprocess.Popen(
+                command,
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                cwd=os.getcwd() if cwd is None else cwd,
+            )
+
+            # Poll process for new output until finished
+            while True:
+                nextline = process.stdout.readline().decode("UTF-8")
+                if nextline == "" and process.poll() is not None:
+                    break
+                sys.stdout.write(nextline)
+                sys.stdout.flush()
+
+            output = process.communicate()[0]
+            exit_code = process.returncode
+
+            if exit_code == 0:
+                print(output)
+                return output
+            else:
+                raise Exception(command, exit_code, output)
 
     @staticmethod
     def download_file(
@@ -117,21 +147,43 @@ class Project:
         with zipfile.ZipFile(zip_file_path, "r") as zip_file:
             zip_file.extractall(destination_dir_path)
 
-    def download_python_dist(
-        self, download_dir_path: Path, python_version: str
-    ):
+    @staticmethod
+    def _download_python_dist(download_path: Path, python_version: str):
         embedded_file_name = f"python-{python_version}-embed-amd64.zip"
-        embedded_file_path = download_dir_path / embedded_file_name
+        embedded_file_path = download_path / embedded_file_name
         with log("Downloading embedde python."):
             if embedded_file_path.is_file():
                 return embedded_file_path
-            self.download_file(
+            Project.download_file(
                 url=f"{PYTHON_URL}/{python_version}/{embedded_file_name}",
                 local_file_path=embedded_file_path,
             )
             if not embedded_file_path.is_file():
                 raise RuntimeError("Download failed!")
         return embedded_file_path
+
+    @staticmethod
+    def _download_getpippy(download_path: Path) -> Path:
+        getpippy_file_path = download_path / "get-pip.py"
+        with log("Downloading `get-pip.py`."):
+            if getpippy_file_path.exists():
+                return getpippy_file_path
+            Project.download_file(
+                url=GET_PIP_URL, local_file_path=getpippy_file_path
+            )
+            if not getpippy_file_path.exists():
+                raise RuntimeError("Download failed!")
+        return getpippy_file_path
+
+    def _make_empty_build_dir(self) -> None:
+        # Delete build folder if it exists
+        if self._build_path.is_dir():
+            with log(
+                f"Existing build directory found, "
+                f"removing contents from `{self._build_path}`"
+            ):
+                shutil.rmtree(self._build_path)
+        self._build_path.mkdir()
 
     def extract_embedded(self, embedded_file_path: Path) -> None:
         with log(
@@ -141,18 +193,6 @@ class Project:
                 zip_file_path=embedded_file_path,
                 destination_dir_path=self._pydist_path,
             )
-
-    def download_getpippy(self, download_dir_path: Path) -> Path:
-        getpippy_file_path = download_dir_path / "get-pip.py"
-        with log("Downloading `get-pip.py`."):
-            if getpippy_file_path.exists():
-                return getpippy_file_path
-            self.download_file(
-                url=GET_PIP_URL, local_file_path=getpippy_file_path
-            )
-            if not getpippy_file_path.exists():
-                raise RuntimeError("Download failed!")
-        return getpippy_file_path
 
     def _copy_source_files(self) -> None:
         ignore_patterns = []
@@ -172,7 +212,7 @@ class Project:
             )
 
     def _patch_pth_file(self, python_version: str) -> None:
-        short_version = Project.get_short_version(python_version)
+        short_version = Project._get_short_version(python_version)
         pth_file_name = f"python{short_version}._pth"  # python39._pth
         pth_file_path = self._pydist_path / pth_file_name
         pythonzip_file_name = f"python{short_version}.zip"  # python39.zip
@@ -193,23 +233,128 @@ class Project:
             )
             pth_file_path.write_text(pth_file_content, encoding="utf8")
 
-    def _extract_pythonzip(self, python_version:str)->None:
+    def _extract_pythonzip(self, python_version: str) -> None:
         """Extract pythonXX.zip zip file to pythonXX.zip folder
         and delete pythonXX.zip zip file
         """
-        short_version = Project.get_short_version(python_version)
+        short_version = Project._get_short_version(python_version)
         pythonzip_file_name = f"python{short_version}.zip"  # python39.zip
         pythonzip_file_path = self._pydist_path / pythonzip_file_name
         pythonzip_dir_path = Path(pythonzip_file_path)
-        with log(f"Extracting `{pythonzip_file_path}` to `{pythonzip_dir_path}`"):
+        with log(
+            f"Extracting `{pythonzip_file_path}` to `{pythonzip_dir_path}`"
+        ):
             pythonzip_file_path = pythonzip_file_path.rename(
                 pythonzip_file_path.with_suffix(".temp_zip")
             )
             Project.unzip(pythonzip_file_path, pythonzip_dir_path)
             pythonzip_file_path.unlink()
 
+    def _install_pip(self) -> None:
+        with log("Installing `pip`"):
+            Project._execute_os_command(
+                command="python.exe get-pip.py --no-warn-script-location",
+                cwd=str(self._pydist_path),
+            )
+            if not (self._pydist_path / "Scripts").exists():
+                raise RuntimeError("Can not install `pip` with `get-pip.py`!")
 
+    def _install_requirements(
+        self,
+        requirements_file_path: Path,
+        extra_pip_install_args: list[str],
+    ):
+        """
+        Install the modules from requirements.txt file
+        - extra_pip_install_args (optional `List[str]`) :
+        pass these additional arguments to the pip install command
+        """
 
+        with log("Installing requirements"):
+            scripts_dir_path = self._pydist_path / "Scripts"
+
+            if extra_pip_install_args:
+                extra_args_str = " " + " ".join(extra_pip_install_args)
+            else:
+                extra_args_str = ""
+
+            try:
+                cmd = (
+                    "pip3.exe install "
+                    + "--no-cache-dir --no-warn-script-location "
+                    + f"-r {str(requirements_file_path)}{extra_args_str}"
+                )
+                Project._execute_os_command(
+                    command=cmd, cwd=str(scripts_dir_path)
+                )
+                return
+            except Exception:
+                print("Installing modules one by one")
+                modules = requirements_file_path.read_text().splitlines()
+                for module in modules:
+                    try:
+                        print(f"Installing {module} ...", end="", flush=True)
+                        cmd = "pip3.exe install --no-cache-dir "
+                        f"--no-warn-script-location {module}{extra_args_str}"
+                        Project._execute_os_command(
+                            command=cmd, cwd=str(scripts_dir_path)
+                        )
+                        print("done")
+                    except Exception:
+                        print("FAILED TO INSTALL ", module)
+                        with (
+                            self._build_path / "FAILED_TO_INSTALL_MODULES.txt"
+                        ).open(mode="a") as f:
+                            f.write(module + "\n")
+                    print("\n")
+
+    def _make_startup_exe(
+        self,
+        show_console: bool,
+        icon_file_path: Union[Path, None],
+    ) -> Path:
+        """Make the startup exe file needed to run the script"""
+
+        relative_pydist_dir = self._pydist_path.relative_to(self._build_path)
+        relative_source_dir = self._source_path.relative_to(self._build_path)
+        exe_file_path = self._build_path / Path(self._main_file).with_suffix(
+            ".exe"
+        )
+        python_entrypoint = "python.exe"
+        command_str = (
+            f"{{EXE_DIR}}\\{relative_pydist_dir}\\{python_entrypoint} "
+            + f"{{EXE_DIR}}\\{relative_source_dir}\\{self._main_file}"
+        )
+        with log(f"Making startup exe file `{exe_file_path}`"):
+            generate_exe(
+                target=exe_file_path,
+                command=command_str,
+                icon_file=icon_file_path,
+                show_console=show_console,
+            )
+
+            if not show_console:
+                main_file_path = self._source_path / self._main_file
+                main_file_content = main_file_path.read_text(
+                    encoding="utf8", errors="surrogateescape"
+                )
+                if HEADER_NO_CONSOLE not in main_file_content:
+                    main_file_path.write_text(
+                        str(HEADER_NO_CONSOLE + main_file_content),
+                        encoding="utf8",
+                        errors="surrogateescape",
+                    )
+
+            self._exe_path = exe_file_path
+            return exe_file_path
+
+    def _rename_exe_file(self, new_file_name: str) -> Path:
+        if new_file_name.lower().endswith(".exe"):  # new_name.exe -> new_name
+            new_file_name = new_file_name.lower().rstrip(".exe")
+        new_exe_path = self.exe_path.with_stem(new_file_name)
+        with log(f"Renaming {self.exe_path} -> {new_exe_path}"):
+            self._exe_path = self._exe_path.rename(new_exe_path)
+        return self.exe_path
 
     def build(
         self,
@@ -221,6 +366,7 @@ class Project:
         source_dir: str = None,
         # TODO ignore_input: Iterable[str] = (),
         show_console: bool = False,
+        exe_name: str = None,
         icon_file: Union[str, Path, None] = None,
         # TODO: download_dir: Union[str, Path] = None,
     ) -> None:
@@ -249,13 +395,13 @@ class Project:
 
         # download embedded python and extract it to build directory
         download_path = Path.home() / "Downloads"
-        embedded_file_path = self.download_python_dist(
-            download_dir_path=download_path, python_version=python_version
+        embedded_file_path = self._download_python_dist(
+            download_path=download_path, python_version=python_version
         )
         self.extract_embedded(embedded_file_path)
 
         # download `get_pip.py` and copy it to build directory
-        getpippy_file_path = self.download_getpippy(
+        getpippy_file_path = self._download_getpippy(
             download_path=download_path
         )
         with log(
@@ -265,3 +411,22 @@ class Project:
 
         self._patch_pth_file(python_version=python_version)
         self._extract_pythonzip(python_version=python_version)
+
+        self._install_pip()
+        self._install_requirements(
+            requirements_file_path=self._requirements_path,
+            extra_pip_install_args=list(extra_pip_install_args),
+        )
+
+        icon_file_path = Path(icon_file) if icon_file is not None else None
+        self._make_startup_exe(
+            show_console=show_console, icon_file_path=icon_file_path
+        )
+
+        if exe_name is not None:
+            self._rename_exe_file(new_file_name=exe_name)
+
+        print(
+            f"\nBuild done! Folder `{self._build_path}` "
+            "contains your runnable application!\n"
+        )
